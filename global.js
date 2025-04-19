d61e8c48c32908679056492f3c4c2282761e9df1
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2025-04-18 19:08:02 -0700

    working on lab03

--- global.js
+++ global.js
@@ -23,7 +23,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
-    : "/website/";         // GitHub Pages repo name
+    : "/portfolio/";         // GitHub Pages repo name
 
 for (let p of pages) {
     let url = p.url;
