75e84c12bd5e88b8f218d62e490621f938b02006
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2025-04-18 21:39:59 -0700

    working on lab03

--- global.js
+++ global.js
@@ -58,11 +58,6 @@ document.body.insertAdjacentHTML(
       </label>`,
 );
 
-if ("colorScheme" in localStorage) {
-    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
-    select.value = localStorage.colorScheme;
-}
-
 let select = document.querySelector('select');
 
 select.addEventListener('input', function (event) {
@@ -70,4 +65,9 @@ select.addEventListener('input', function (event) {
     document.documentElement.style.setProperty('color-scheme', event.target.value);
     localStorage.colorScheme = event.target.value;
 });
+
+if ("colorScheme" in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
 
