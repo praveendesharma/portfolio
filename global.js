7b0bfee1deae771970640aa7114514daf43ef165
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2025-04-18 19:14:44 -0700

    working on lab03

--- global.js
+++ global.js
@@ -18,23 +18,18 @@ let pages = [
     { url: 'https://github.com/praveendesharma', title: 'GitHub' }
 ];
 
-// Step 2: Create the <nav> element and add it to the top of the body
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-// Step 3: Determine the base path depending on whether we’re local or on GitHub Pages
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
-    : "/portfolio/";         // Replace 'website' with your GitHub repo name
+    : "/portfolio/";         // GitHub Pages repo name
 
-// Step 4: Loop through the pages and add links to the <nav>
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
 
-    // If it's a relative URL, prepend the base path
     url = !url.startsWith('http') ? BASE_PATH + url : url;
 
-    // Add the link to the <nav>
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
