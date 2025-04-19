2c7e1ddbbef30bc00a3bd18aa239a9305813b92d
Praveen Sharma <80846168+praveendesharma@users.noreply.github.com>
2025-04-18 18:53:36 -0700

    working on lab03

--- global.js
+++ global.js
@@ -21,9 +21,14 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     // next step: create link and add it to nav
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-  }
+}
